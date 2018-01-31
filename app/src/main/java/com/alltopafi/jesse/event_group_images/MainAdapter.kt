03d9a89beb4d9d41cda5eaa554d1b5f477ad71b0
alltopafi <alltopafi@gmail.com>
2018-01-30 19:34:23 -0600

    Initial commit

---
+++ app/src/main/java/com/alltopafi/jesse/event_group_images/MainAdapter.kt
@@ -0,0 +1,31 @@
+package com.alltopafi.jesse.event_group_images
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.image_row.view.*
+
+/**
+ * Created by Jesse on 1/30/18.
+ */
+
+class MainAdapter: RecyclerView.Adapter<CustomViewHolder>() {
+
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): CustomViewHolder {
+        val layoutInflater = LayoutInflater.from(parent?.context)
+        val cellForRow = layoutInflater.inflate(R.layout.image_row, parent, false)
+
+        return CustomViewHolder(cellForRow)
+    }
+
+    override fun onBindViewHolder(holder: CustomViewHolder?, position: Int) {
+        holder?.view?.textView_authorName?.text = "dsfsd"
+    }
+}
+
+class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view)
