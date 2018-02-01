1752c0dba274c8474eb3566e473eb78d033bd409
alltopafi <alltopafi@gmail.com>
2018-01-31 20:49:02 -0600

    retrieve from firebase moved to MainActivity(should be on it's own thread) and that array is passed to the recycleview adapter

--- app/src/main/java/com/alltopafi/jesse/event_group_images/MainActivity.kt
+++ app/src/main/java/com/alltopafi/jesse/event_group_images/MainActivity.kt
@@ -3,7 +3,12 @@ package com.alltopafi.jesse.event_group_images
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
+import com.alltopafi.jesse.event_group_images.constant.Constants
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,10 +16,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
 
         recyclerView_main.layoutManager = LinearLayoutManager(this )
-        recyclerView_main.adapter = MainAdapter()
+        val posts = firebaseRetrieve()
+        recyclerView_main.adapter = MainAdapter(posts)
 
     }
+
+    private fun firebaseRetrieve(): ArrayList<Post?> {
+        val posts = ArrayList<Post?>()
+
+        FirebaseDatabase.getInstance().reference
+                .child(Constants.ROOT_NODE).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot?) {
+
+
+                for(postSnapshot in p0!!.children) {
+                    Log.i("Key value for child", postSnapshot.key)
+
+                    val post = p0.child(postSnapshot.key).getValue(Post::class.java)
+                    posts.add(post)
+                }
+            }
+
+            override fun onCancelled(p0: DatabaseError?) {
+                Log.e("Error", p0.toString())
+            }
+        })
+
+        return posts
+    }
+
 }
--- app/src/main/java/com/alltopafi/jesse/event_group_images/MainAdapter.kt
+++ app/src/main/java/com/alltopafi/jesse/event_group_images/MainAdapter.kt
@@ -1,26 +1,16 @@
 package com.alltopafi.jesse.event_group_images
 
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.alltopafi.jesse.event_group_images.constant.Constants
-import com.google.firebase.FirebaseException
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.image_row.view.*
 
 /**
  * Created by Jesse on 1/30/18.
  */
 
-class MainAdapter: RecyclerView.Adapter<CustomViewHolder>() {
-
-    val posts = firebaseRetrieve()
-
+class MainAdapter(val posts: ArrayList<Post?>): RecyclerView.Adapter<CustomViewHolder>(){
 
 
     override fun getItemCount(): Int {
@@ -39,22 +29,5 @@ class MainAdapter: RecyclerView.Adapter<CustomViewHolder>() {
 
     }
 }
-
- private fun firebaseRetrieve(): ArrayList<Post?> {
-    val posts = ArrayList<Post?>()
-
-     FirebaseDatabase.getInstance().reference
-             .child(Constants.ROOT_NODE).addValueEventListener(object : ValueEventListener{
-         override fun onDataChange(p0: DataSnapshot?) {
-            val post = p0?.getValue(Post::class.java)
-            posts.add(post)
-         }
-
-         override fun onCancelled(p0: DatabaseError?) {
-             Log.e("Error", p0.toString())
-         }
-     })
-     return posts
- }
 
 class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view)
