a4fe68bcf3268fe6e1d6ece1c8640e9e944061ae
alltopafi <alltopafi@gmail.com>
2018-02-02 20:45:40 -0600

    added update to view when firebase callback returns

--- app/src/main/java/com/alltopafi/jesse/event_group_images/MainActivity.kt
+++ app/src/main/java/com/alltopafi/jesse/event_group_images/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.alltopafi.jesse.event_group_images
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
+import android.widget.Button
 import com.alltopafi.jesse.event_group_images.constant.Constants
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -21,6 +26,14 @@ class MainActivity : AppCompatActivity() {
         val posts = firebaseRetrieve()
         recyclerView_main.adapter = MainAdapter(posts)
 
+
+        val cameraButton = findViewById<Button>(R.id.button_camera)
+        cameraButton.setOnClickListener {view ->
+
+            val cameraViewIntent = Intent(this, ImageCaptureActivity::class.java)
+            startActivity(cameraViewIntent)
+        }
+
     }
 
     private fun firebaseRetrieve(): ArrayList<Post?> {
@@ -28,22 +41,23 @@ class MainActivity : AppCompatActivity() {
 
         FirebaseDatabase.getInstance().reference
                 .child(Constants.ROOT_NODE).child(Constants.EVENT_NODE).child(Constants.POST_NODE).
-                                                    addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(p0: DataSnapshot?) {
+                addValueEventListener(object : ValueEventListener {
+                    override fun onDataChange(p0: DataSnapshot?) {
 
 
-                for(postSnapshot in p0!!.children) {
-                    Log.i("Key value for child", postSnapshot.key)
+                        for (postSnapshot in p0!!.children) {
+                            Log.i("Key value for child", postSnapshot.key)
 
-                    val post = p0.child(postSnapshot.key).getValue(Post::class.java)
-                    posts.add(post)
-                }
-            }
+                            val post = p0.child(postSnapshot.key).getValue(Post::class.java)
+                            posts.add(post)
+                        }
+                        recyclerView_main.adapter.notifyDataSetChanged()
+                    }
 
-            override fun onCancelled(p0: DatabaseError?) {
-                Log.e("Error", p0.toString())
-            }
-        })
+                    override fun onCancelled(p0: DatabaseError?) {
+                        Log.e("Error", p0.toString())
+                    }
+                })
 
         return posts
     }
